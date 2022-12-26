e6cb7a12191481f4a8dac6117f0991d42ede5c84
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-12-26 19:31:47 +0300

    exercise

--- src/test/java/Locators3.java
+++ src/test/java/Locators3.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Locators3 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Locators3 {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/MBB/Desktop/SDET/driver/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+        System.out.println(driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).getText());
+
+    }
+
+
 }
