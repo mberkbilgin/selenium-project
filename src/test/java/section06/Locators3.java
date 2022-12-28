8d2abf47c93c812780eb8ebbb958b6a1dcd70e1b
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-12-28 18:13:39 +0300

    StaticDropdown

--- src/test/java/section06/Locators3.java
+++ src/test/java/section06/Locators3.java
@@ -1,18 +1,24 @@
+package section06;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class WindowActivities {
+public class Locators3 {
     public static void main(String[] args) {
+
         System.setProperty("webdriver.chrome.driver", "/Users/MBB/Desktop/SDET/driver/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.manage().window().maximize();
-        driver.get("http://google.com");
-        driver.navigate().to("https://rahulshettyacademy.com/");
-        driver.navigate().back();
-        driver.navigate().forward();
+
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+        System.out.println(driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).getText());
+
     }
+
+
 }
