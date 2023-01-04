fe1d8e3677d3c898949825bd167b9945e3a23710
M.Berk Bilgin <m.berkbilgin@outlook.com>
2023-01-04 22:46:18 +0300

    LiveDemo.java

---
+++ src/main/java/exercises/section14/LiveDemo.java
@@ -0,0 +1,44 @@
+package exercises.section14;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LiveDemo {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/MBB/Desktop/SDET/driver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+        driver.findElement(By.xpath("//tr/th[1]")).click();
+        List<WebElement> elementsList = driver.findElements(By.xpath("//tr/td[1]"));
+
+        List<String> originalList = elementsList.stream().map(s -> s.getText()).collect(Collectors.toList());
+
+        List<String> sortedList = originalList.stream().sorted().collect(Collectors.toList());
+
+        Assert.assertTrue(originalList.equals(sortedList));
+
+        List<String> price;
+        do {
+            List<WebElement> rows = driver.findElements(By.xpath("//tr/td[1]"));
+            price = rows.stream().filter(s -> s.getText().contains("Rice"))
+                    .map(s -> getPriceVeggie(s)).collect(Collectors.toList());
+            price.forEach(a -> System.out.println(a));
+            if (price.size() < 1) {
+                driver.findElement(By.cssSelector("a[aria-label='Next']")).click();
+            }
+        } while (price.size() < 1);
+    }
+
+    private static String getPriceVeggie(WebElement s) {
+        String priceValue = s.findElement(By.xpath("following-sibling::td[1]")).getText();
+
+        return priceValue;
+    }
+}
