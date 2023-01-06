40b7ae179ad9d2d6d2c11f1bdc68ab857ec2bf86
M.Berk Bilgin <m.berkbilgin@outlook.com>
2023-01-06 21:47:24 +0300

    section18/

---
+++ src/main/java/exercises/section18/LandingPage.java
@@ -0,0 +1,39 @@
+package exercises.section18;
+
+import exercises.section17.abstractComponents.AbstractComponent;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LandingPage extends AbstractComponent {
+    WebDriver driver;
+
+    public LandingPage(WebDriver driver) {
+        super(driver);
+        this.driver= driver;
+        PageFactory.initElements(driver,this);
+    }
+
+
+    @FindBy(id = "userEmail")
+    WebElement userEmail;
+
+    @FindBy(id = "userPassword")
+    WebElement passwordEle;
+
+    @FindBy(id = "login")
+    WebElement submit;
+
+    public ProductCatalogue loginApplication(String email, String password){
+    userEmail.sendKeys(email);
+    passwordEle.sendKeys(password);
+    submit.click();
+    ProductCatalogue productCatalogue = new ProductCatalogue(driver);
+    return productCatalogue;
+    }
+    public void goTo(){
+        driver.get("https://rahulshettyacademy.com/client");
+    }
+
+}
